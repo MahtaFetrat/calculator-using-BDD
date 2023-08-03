f996106bcae33077b5ce97b8f0c0d2420add0060
msrazavi <razavi.mst@gmail.com>
2023-08-03 23:00:24 +0330

    add pow to calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,4 +9,14 @@ public class Calculator {
         if (b == 0) return Double.MAX_VALUE;
         return (double) a / b;
     }
+
+    public double pow(int a, int b) {
+        double base = (b >= 0 ? (double) a :  (double) 1 / a);
+        b = Math.abs(b);
+        double result = 1;
+        for (int i = 0; i < b; i ++) {
+            result = mult(result, base);
+        }
+        return result;
+    }
 }
