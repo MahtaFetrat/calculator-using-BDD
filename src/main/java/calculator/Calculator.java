df1dc6f5393645c67108ea894cca539a1a2df7fb
msrazavi <razavi.mst@gmail.com>
2023-08-03 22:57:13 +0330

    add div to calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,9 @@ public class Calculator {
     public double mult(double a, double b) {
         return a * b;
     }
+
+    public double div(int a, int b) {
+        if (b == 0) return Double.MAX_VALUE;
+        return (double) a / b;
+    }
 }
